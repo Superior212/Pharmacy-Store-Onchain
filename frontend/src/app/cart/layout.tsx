1aba2fe59355f7daa23791eefc47ba34ab5ca9ab
LayintonDev <layintondev@gmail.com>
2024-10-15 00:37:44 +0100

    pages refactoring

--- frontend/src/app/cart/layout.tsx
+++ frontend/src/app/cart/layout.tsx
@@ -1,12 +1,18 @@
 import Sidebar from "@/components/Cart/Sidebar"
+import { Navbar } from "@/components/Layout"
+import "../globals.css";
 
 
 
 const CartLayout = ({ children }: { children: React.ReactNode }) => {
 
     return (
-        <main className="max-w-[80rem] h-screen mx-auto px-4 sm:px-6 lg:px-8 bg-[#FAFAFA] rounded-[0.625rem]">
-            <h3 className="font-bold text-[2.5rem] text-[#5C5B5C] mt-2 lg:mt-[2.81rem]">Cart</h3>
+        <html>
+             <body
+        className={`antialiased flex flex-col justify-between min-h-screen`}>
+        <Navbar />
+        <main className="w-full lg:h-screen mx-auto px-4 sm:px-6 lg:px-8 bg-[#FAFAFA] rounded-[0.625rem]">
+            <h3 className="font-bold text-[2.5rem] text-[#5C5B5C]  lg:mt-[2.81rem]">Cart</h3>
             <hr className="w-full h-[0.5px] bg-[#D3D3D3] border-b" />
             <section className="w-full flex flex-col lg:flex-row mt-[17px]">
                  <Sidebar/>
@@ -17,6 +23,8 @@ const CartLayout = ({ children }: { children: React.ReactNode }) => {
 
 
         </main>
+        </body>
+        </html>
     )
 }
 
