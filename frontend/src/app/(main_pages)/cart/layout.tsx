9948e788f275bdcb8b557cacf6d217f4fc55c2bf
Akande Ozavize <akandeozavize@gmail.com>
2024-10-16 00:12:38 +0100

    wagmi provider updates

--- frontend/src/app/(main_pages)/cart/layout.tsx
+++ frontend/src/app/(main_pages)/cart/layout.tsx
@@ -6,8 +6,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   
-
         <main className="container lg:h-screen mx-auto px-4 sm:px-6 lg:px-8 bg-[#FAFAFA] rounded-[0.625rem]">
             <h3 className="font-bold text-[2.5rem] text-[#5C5B5C]  lg:mt-[2.81rem]">Cart</h3>
             <hr className="w-full h-[0.5px] bg-[#D3D3D3] border-b" />
