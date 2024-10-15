5b2b17a7dfe7a0eb635f22d1820455ec8e02ef95
udodinho <udodinho200@gmail.com>
2024-10-15 16:20:56 +0100

    fix: add role checking before adding prescription

--- SmartContract/contracts/PrescriptionManagement.sol
+++ SmartContract/contracts/PrescriptionManagement.sol
@@ -1,29 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract PrescriptionManagement {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IUserManagement.sol";
+
+contract PrescriptionManagement is Ownable {
 
     struct Prescription {
         address patient;
         address doctor;
         string prescription;
-        bool validated;
+        bool isValidated;
+        bool isUsed;
     }
 
-    mapping(address => string) patientPrescription; // Mapping patient to prescription
+    IUserManagement public userManagement;
 
-    // Mapping from prescription string to Prescription struct
+    mapping(address => string[]) private patientPrescriptions;
     mapping(string => Prescription) private prescriptions;
+    mapping(address => mapping(address => bool)) private pharmacyAccess;
 
-    // Events
-    event PrescriptionCreated(address indexed patient, address indexed doctor, string indexed prescription);
+    event PrescriptionCreated(address indexed patient, address indexed doctor, string prescription);
+    event AccessGranted(address indexed patient, address indexed pharmacy);
+    event PrescriptionUsed(address indexed patient, string prescription);
 
-    // Custom Errors
+    error DoctorNotVerified();
     error InvalidPrescription();
-    error PrescriptionAlreadyExists();
-    error InvalidPrescriptionCode();
-    error NotOwnerOfPrescription();
     error AddressZeroDetected();
+    error NotAuthorizedDoctor();
+    error NoAccessToPrescription();
+    error PrescriptionAlreadyUsed();
+    error InvalidPrescriptionCode();
+    error NotOwnerOfPrescriptionOrAllowedToView();
+
+    constructor(address _userManagementAddress) Ownable(msg.sender) {
+        userManagement = IUserManagement(_userManagementAddress);
+    }
 
     function addressZeroCheck(address _address) internal pure {
         if (_address == address(0)) revert AddressZeroDetected();
@@ -36,45 +48,81 @@ contract PrescriptionManagement {
         addressZeroCheck(msg.sender);
         addressZeroCheck(_patient);
 
-        // Validate the prescription hash
-        if (bytes(_prescriptionHash).length == 0 || bytes(_prescriptionHash).length < 46) {
+        if (bytes(_prescriptionHash).length == 0 || bytes(_prescriptionHash).length < 45) {
             revert InvalidPrescriptionCode();
         }
 
-        Prescription memory _prescription = Prescription({
+        IUserManagement.Role role = userManagement.getRole(msg.sender);
+        if (role != IUserManagement.Role.Doctor) {
+            revert NotAuthorizedDoctor();
+        }
+
+        if (!userManagement.isDoctorVerified(msg.sender)) {
+            revert DoctorNotVerified();
+        }
+
+        prescriptions[_prescriptionHash] = Prescription({
             patient: _patient,
             doctor: msg.sender,
             prescription: _prescriptionHash,
-            validated: false
+            isValidated: false,
+            isUsed: false
         });
 
-        prescriptions[_prescriptionHash] = _prescription;
-        patientPrescription[_patient] = _prescriptionHash;
+        patientPrescriptions[_patient].push(_prescriptionHash);
 
         emit PrescriptionCreated(_patient, msg.sender, _prescriptionHash);
     }
 
-    function validatePrescription(
+    function grantPharmacyAccess(address _pharmacy) public {
+        addressZeroCheck(_pharmacy);
+
+        IUserManagement.Role role = userManagement.getRole(_pharmacy);
+
+        if (role != IUserManagement.Role.Pharmacy) {
+            revert NoAccessToPrescription();
+        }
+
+        pharmacyAccess[msg.sender][_pharmacy] = true;
+
+        emit AccessGranted(msg.sender, _pharmacy);
+    }
+
+    function validateAndUsePrescription(
         string memory _prescriptionHash, 
         address _patient
-    ) public view {
+    ) public {
         addressZeroCheck(msg.sender);
         addressZeroCheck(_patient);
 
-        string memory _prescription = patientPrescription[_patient];
-
-        if (keccak256(abi.encodePacked(_prescription)) != keccak256(abi.encodePacked(_prescriptionHash))) {
-            revert InvalidPrescriptionCode();
+        if (!pharmacyAccess[_patient][msg.sender]) {
+            revert NoAccessToPrescription();
         }
 
-        if (prescriptions[_prescriptionHash].validated) {
-            revert InvalidPrescription();
+        Prescription storage prescription = prescriptions[_prescriptionHash];
+
+        if (prescription.isUsed) {
+            revert PrescriptionAlreadyUsed();
         }
+
+        prescription.isValidated = true;
+        prescription.isUsed = true;
+
+        emit PrescriptionUsed(_patient, _prescriptionHash);
     }
 
-    function getPrescription(string memory _prescriptionHash, address _patient) public view returns (Prescription memory) {
-        validatePrescription(_prescriptionHash, _patient);
-        
-        return prescriptions[_prescriptionHash];
+    function getPrescription(string memory _prescriptionHash, address _patient )
+             public view returns (Prescription memory) {
+
+        Prescription memory prescription = prescriptions[_prescriptionHash];
+
+        if (
+            msg.sender != prescription.doctor &&
+            msg.sender != prescription.patient &&
+            !pharmacyAccess[_patient][msg.sender] &&
+            msg.sender != owner()
+        ) revert NotOwnerOfPrescriptionOrAllowedToView();
+
+        return prescription;
     }
 }
