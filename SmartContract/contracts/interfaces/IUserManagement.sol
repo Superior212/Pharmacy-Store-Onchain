7e0935641c963501772eaf05eda98ef7126ab4e5
udodinho <udodinho200@gmail.com>
2024-10-16 00:13:35 +0100

    fix: change customer to patient role

--- SmartContract/contracts/interfaces/IUserManagement.sol
+++ SmartContract/contracts/interfaces/IUserManagement.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 interface IUserManagement {
-    enum Role { None, Customer, Pharmacy, Doctor }
+    enum Role { None, Patient, Pharmacy, Doctor }
 
     function getRole(address _user) external view returns (Role);
     function isDoctorVerified(address _doctor) external view returns (bool);
