eb54efd1d428125ff6aaadbbd047c37b56acfd66
Superior <samsonaderonmu2021@gmail.com>
2024-10-16 09:35:07 +0100

    Refactor addresses in frontend and smart contract modules

--- SmartContract/ignition/modules/PrescriptionManagement.ts
+++ SmartContract/ignition/modules/PrescriptionManagement.ts
@@ -1,7 +1,7 @@
 // This setup uses Hardhat Ignition to manage smart contract deployments.
 // Learn more about it at https://hardhat.org/ignition
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-const UserManagementAddress = "0x34B5D26FC98f4da8eD68b07F07BEcf3BEA16a9E0";
+const UserManagementAddress = "0x798AA46f2caBdd946e0b0E7192dD973b276B8fAC";
 const PrescriptionModule = buildModule("PrescriptionModule", (m) => {
   const prescription = m.contract("PrescriptionManagement", [
     UserManagementAddress,
