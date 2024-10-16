582a281248db336cd9812ebb5b4230468df92b07
udodinho <udodinho200@gmail.com>
2024-10-16 16:16:53 +0100

    fix: update the market place CA

--- frontend/src/constant/index.ts
+++ frontend/src/constant/index.ts
@@ -3,7 +3,7 @@ import userManagementAbi from "../Abi/UserManagement.json";
 import prescriptionAbi from "../Abi/PrescriptionManagement.json";
 
 export const MarketPlaceContract = {
-  address: "0x8Ee80676854d8d12D563d07210443B9181DfdfcE",
+  address: "0xDb5Cee311F23bd0FA7c1f585bb703721e57457C5",
   abi: marketPlaceAbi,
 };
 
@@ -12,6 +12,6 @@ export const UserManagementContract = {
   abi: userManagementAbi,
 };
 export const PrescriptionManagement = {
-  address: "0x798AA46f2caBdd946e0b0E7192dD973b276B8fAC",
+  address: "0xbaac6B08CA911d6c5e8a84aD8B6EBC0f90C14FCc",
   abi: prescriptionAbi,
 };
