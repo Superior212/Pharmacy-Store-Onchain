b2b854190e6aa1fc713679753dc6b9b19ea49e85
Superior <samsonaderonmu2021@gmail.com>
2024-10-16 23:06:53 +0100

    Refactor package dependencies and update Medication component

--- frontend/src/components/Medication/Medication.tsx
+++ frontend/src/components/Medication/Medication.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 import Hero from "./Hero";
 import Search from "./Search";
 import ProductListing from "./ProductListing";
-import { useReadContract } from "wagmi";
-import { MarketPlaceContract } from "@/constant";
-import MarketplaceAbi from "../../Abi/Marketplace.json";
+// import { useReadContract } from "wagmi";
+// import { MarketPlaceContract } from "@/constant";
+// import MarketplaceAbi from "../../Abi/Marketplace.json";
 
 const Medication = () => {
-  const medicationId = 3;
+  // const medicationId = 3;
 
-  console.log("Medication ID:", medicationId);
+  // console.log("Medication ID:", medicationId);
   // const { data: medicationDetails } = useReadContract({
   //   abi: MarketplaceAbi,
   //   address: MarketPlaceContract.address as `0x${string}`,
