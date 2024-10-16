0072fa6cf78842656dc8e20b0a901201670dd5be
Superior <samsonaderonmu2021@gmail.com>
2024-10-16 03:21:42 +0100

    Refactor CSS import and update config for PharmaX app

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { liskSepolia } from "wagmi/chains";
+import { lisk, liskSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "PharmaX",
   projectId: "53254d53b6eb744328cd68c4b4e096cc",
-  chains: [liskSepolia],
+  chains: [lisk, liskSepolia],
   ssr: true,
 });
